30e00434532c28bf7a6d2998a13f8098cfae0c46
vicbguti-espol <vicbguti@espol.edu.ec>
2023-10-29 13:54:45 -0500

    Companies

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./routes/Home";
 import Explore from "./routes/Explore";
 import ChatBot from "./routes/ChatBot";
-import Store from "./routes/Store";
 import StoreHandler from "./util/StoreHandler";
 
 function App() {
--- src/routes/Explore.jsx
+++ src/routes/Explore.jsx
@@ -1,15 +1,29 @@
 import Scene from "../components/Scene";
+import { Link } from "react-router-dom";
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://aotfwsxkljvqjnsckobf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvdGZ3c3hrbGp2cWpuc2Nrb2JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1MTU3NTgsImV4cCI6MjAxNDA5MTc1OH0.EcrRFzIBjnSRRSvUr9OGcqrklO6csZ6cdPD2pEzrbVQ')
+
+let { data: stores, error } = await supabase
+                                .from('store')
+                                .select('*')
 
 function Explore() {
+    const itemsNames = stores.map(store =>
+        <li key={store.store_id}>
+            <button>
+                {store.store_name}    
+            </button>
+        </li>
+        );
+        
     return (
         <>
-            <ul>
-                <li>
-                    
-                </li>
-            </ul>
+            <ul>{itemsNames}</ul>
         </>
-    )
+    );
 }
 
 export default Explore;
